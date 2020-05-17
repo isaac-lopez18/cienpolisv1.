ce738654c74751282b35c6662a76da3d3ee88bfc
isaac-lopez18 <65476868+isaac-lopez18@users.noreply.github.com>
2020-05-17 16:26:50 -0500

    Create cinepolis.java

---
+++ cinepolis.java
@@ -0,0 +1,29 @@
+//pedir usuario usuario, 1.ventas 2.inventarios 3.
+
+import java.util.Scanner;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+/Inventario Cinepolis//
+
+public class cinepolis{
+  //presentacion
+    JPanel panelPresentacion;
+    JButton iniciar;
+    JLabel fondoPresentacion;
+    ImageIcon imagenFondoPres;
+  
+  JFrame ventana;
+  
+  
+  public class Menu(){
+  
+        ventana = new JFrame("INVENTARIOS");
+        ventana.setSize(700, 700);
+        ventana.setLayout(null);
+        ventana.setLocationRelativeTo(null);
+        ventana.setResizable(false);
+  
+  }
+    }
+ 
